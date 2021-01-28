c44d68af474316b2ca422ab68ed75c041261a764
UKHasantha <hasanthaijse@gmail.com>
2021-01-28 23:32:50 +0530

    main page list view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CardComponent } from './card/card.component';
 import { ItemComponent } from './item/item.component';
 
 
 const routes: Routes = [
   { path: 'route-item', component: ItemComponent },
+  { path: 'route-card', component: CardComponent },
   { path: '', redirectTo: '/route-item', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,29 @@ import { ItemComponent } from './item/item.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Itemservice } from './services/itemservice';
+import { CardComponent } from './card/card.component';
+import { SignComponent } from './sign/sign.component';
+import { LoginComponent } from './login/login.component';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemComponent
+    ItemComponent,
+    CardComponent,
+    SignComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MDBBootstrapModule.forRoot()
+     
   ],
   providers: [
     Itemservice
